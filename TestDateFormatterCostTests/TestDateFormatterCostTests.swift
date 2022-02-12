bb04c6a19572775c398c524253e40bc625be479d
yanghojoon <yanghojoon8487@gmail.com>
2022-02-12 16:55:52 +0900

    test: dateFormatter를 사용할 때 소요되는 시간 검증
    - 테스트 시간이 너무 길어져 반복 횟수를 1만회로 수정
    - dateFomatter의 생성을 반복할 지, 한 번만 할 지 구분하여 테스트
    - date(from:)과 string(from:) 메서드도 비교

--- TestDateFormatterCostTests/TestDateFormatterCostTests.swift
+++ TestDateFormatterCostTests/TestDateFormatterCostTests.swift
@@ -9,28 +9,70 @@ import XCTest
 
 class TestDateFormatterCostTests: XCTestCase {
 
-    let numberOfIterations = 1000000
+    let numberOfIterations = 10000
     
-    func test_dateFormatterCreation() { // 0.435초
+    // MARK: - 인스턴스 생성에 소요되는 시간 검증
+    func test_dateFormatterCreation() { // 0.005초
         self.measure {
             for _ in (0..<numberOfIterations) {
                 let dateFormatter = DateFormatter()
             }
         }
     }
-    
-    func test_dateCreation() { // 0.209초
+
+    func test_dateCreation() { // 0.003초
         self.measure {
             for _ in (0..<numberOfIterations) {
                 let date = Date()
             }
         }
     }
-    
-    func test_viewCreation() { // 1.829초 
+
+    func test_viewCreation() { // 0.019초
         self.measure {
             for _ in (0..<numberOfIterations) {
                 let view = UIView()
+            }
+        }
+    }
+    
+    // MARK: - 사용할 때 소요되는 시간 검증
+    func test_dateFormatterCreationAndMakeString() { // 0.181
+        self.measure {
+            for _ in (0..<numberOfIterations) {
+                let date = Date()
+                let dateFormatter = DateFormatter() // 매번 DateFormatter를 새롭게 생성
+                let dateToString = dateFormatter.string(from: date)
+            }
+        }
+    }
+    
+    func test_dateFormatterCreationOnceAndMakeString() { // 0.005
+        let dateFormatter = DateFormatter()
+        
+        self.measure {
+            for _ in (0..<numberOfIterations) {
+                let date = Date()
+                let dateToString = dateFormatter.string(from: date)
+            }
+        }
+    }
+    
+    func test_dateFormatterCreationAndMakeDate() { // 0.313
+        self.measure {
+            for _ in (0..<numberOfIterations) {
+                let dateFormatter = DateFormatter() // 매번 DateFormatter를 새롭게 생성
+                let dateToString = dateFormatter.date(from: "30/01/2020")
+            }
+        }
+    }
+    
+    func test_dateFormatterCreationOnceAndMakeDate() { // 0.134
+        let dateFormatter = DateFormatter()
+        
+        self.measure {
+            for _ in (0..<numberOfIterations) {
+                let dateToString = dateFormatter.date(from: "30/01/2020")
             }
         }
     }
